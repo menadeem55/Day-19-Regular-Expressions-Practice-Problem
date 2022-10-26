76de5d86cef721a5bf01bcaabca6c7fad5ec496e
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2022-10-27 00:46:26 +0530

    [Added: First Name validation]

--- src/com/regex/java/userRegistration.java
+++ src/com/regex/java/userRegistration.java
@@ -4,8 +4,20 @@ import java.util.*;
 import java.util.regex.Pattern;
 
 public class userRegistration {
+
+	public boolean userName(String regex, String input) {
+		return Pattern.compile(regex).matcher(input).matches();
+
+	}
+
 	public static void main(String[] args) {
-		System.out.println("Welcome to regex Problem");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter First Name: ");
+		String firstName = "^[A-Z]{1}[a-z]{2,}";		// Nadeem = true,	// nadeem = false
+		String input = sc.next();
+		userRegistration ur = new userRegistration();
+		System.out.println(ur.userName(firstName, input)); 
+															
 
 	}
 
