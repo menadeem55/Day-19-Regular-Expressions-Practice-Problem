7abed15e0c3bd0c7fba29ed5c96cb43db104b4a6
Nadeem Akhtar <111959123+menadeem55@users.noreply.github.com>
2022-10-27 00:55:05 +0530

    Update userRegistration.java

--- src/com/regex/java/userRegistration.java
+++ src/com/regex/java/userRegistration.java
@@ -13,7 +13,7 @@ public class userRegistration {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter First Name: ");
-		String firstName = "^[A-Z]{1}[a-z]{2,}";		// Nadeem = true,	// nadeem = false
+		String firstName = "^[A-Z]{1}[a-z]{2,}$";		// Nadeem = true,	// nadeem = false
 		String input = sc.next();
 		userRegistration ur = new userRegistration();
 		System.out.println(ur.userName(firstName, input)); 
