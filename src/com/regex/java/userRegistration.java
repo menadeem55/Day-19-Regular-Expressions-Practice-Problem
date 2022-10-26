96698a8c473b159201557225a8b00fbcdc3089b3
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2022-10-27 00:52:54 +0530

    [Added: Last Name validation]

--- src/com/regex/java/userRegistration.java
+++ src/com/regex/java/userRegistration.java
@@ -13,10 +13,16 @@ public class userRegistration {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter First Name: ");
-		String firstName = "^[A-Z]{1}[a-z]{2,}";		// Nadeem = true,	// nadeem = false
-		String input = sc.next();
+		String firstName = "^[A-Z]{1}[a-z]{2,}$";		// Nadeem = true,	// nadeem = false
+		String inputfirstName = sc.next();
+		
+		System.out.println("Enter Last Name: ");
+		String lastName = "^[A-Z]{1}[a-z]{2,}$";		//Akhtar = true,	//akhtar = false
+		String inputLastName = sc.next();
+		
 		userRegistration ur = new userRegistration();
-		System.out.println(ur.userName(firstName, input)); 
+		System.out.println(ur.userName(firstName, inputfirstName)); 
+		System.out.println(ur.userName(lastName, inputLastName));
 															
 
 	}
